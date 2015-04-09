a34c10b6c656d3b7b4bccd5275c3849a4294fd7b
Shreejith <kvn320@gmail.com>
2015-04-09 22:02:05 +0530

    Changes by PHANI

---
+++ mission.php
@@ -0,0 +1,44 @@
+
+
+
+<div class="jumbotron no-margin">
+<div class="container"
+<p>
+<h3>Our Strengths</h3>
+<h4>	
+Our Services are sustained by personalized customer care, fast turnaround, quality support and low cost value added services 
+with global Quality at local cost. The Company has the competence to perform Software Development and 
+Web Development using latest technologies like Java, Asp, Asp.Net and many more. 
+We develop Software’s Websites according to the requirements of the clients. 
+Our competent designers provide fascinating designs for your website, software, logos, brochures and other marketing materials.
+</h4>
+</p>
+</div>
+<div class="container">
+	<div class="row">
+		<div class="col-xs-6 col-md-6">
+			<p>
+				<h3>Mission</h3>
+				<h4>Airreight aims to become a leading and effective IT service provider. At Airreight, our highly talented and
+ 				empowered team delivers reliable, scalable, best-in-class, and cost effective business solutions in various domains by 
+ 				leveraging technologies and innovative processes. We aim to go 'Above and Beyond' in the Technology Solutions, 
+				Application Development, Web development services and Managed Infrastructure Services to help simplify your business.
+				</h4>
+			</p>
+		</div>
+		<div class="col-xs-6 col-md-6">
+			<p>
+			<h3>Vision</h3>
+			<h4>We value innovation. Our ethics is to reinforce the core values of our clients. 
+			We endeavor to rise above all and apply the best talent to help our clients achieve high performance outcomes.
+		 	Our goal is to be recognized as the Indian and the global leader in the business community.
+			</h4>
+			</p>
+		</div>
+		
+		
+
+
+	</div>
+</div>
+</div>
---
+++ technologies.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php include("header.php"); ?>
+</head>
+<body>
+<?php include("navbar.php"); ?>
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+<div class="container">
+  <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <img src="bootstrap/images/1.jpg" alt="nothing">
+        <div class="carousel-caption">
+          ...
+        </div>
+      </div>
+
+  </div>
+ </div>   
+</div>
+<div class="jumbotron no-margin">
+<div class="container">
+JAVA
+Airreight has been working on Java and related technologies since its inception. 
+We had built various enterprise applications using JSP/Servlet technologies. 
+The Java Center of Excellence Initiative (JCEI) of Airreight permits our young engineers to get exposure to core and enterprise Java along 
+with leading Java frameworks, very early in their career.
+
+J2EE
+Airreight develops active web-applications using various application servers and web-containers. 
+Airreight java development team implements J2EE for our client’s custom application development needs and offers platform independence which helps in developing 
+cross-platform portable solutions. Airreight java programmers use brand new Java tools to develop multi-platform applications and 
+enrich client services. Airreight develop user interfaces that have advanced graphics and maintain brand image of the software products/projects.
+
+
+SQL
+Our competence in MS SQL Server, MySQL, as well as commitment to serving our clients translate into top quality and cost-saving solutions with Remote Database Administration. Airreight has wide-ranging practice installing and supporting SQL configurations of all varieties. Our in house SQL DBA's have supported configurations up through advanced clustered solutions for high availability solutions.
+ 
+PHP
+PHP is widely used for developing dynamic, database-driven web pages. 
+Airreight has completed a number of successful projects using PHP / MySQL.
+ Our dedicated PHP team will develop consistent and scalable solutions as per requirements.
+  Airreight PHP/MySQL expertise includes developing large database driven websites, Ecommerce websites with integration of different payment gateways, Integration of PHP based Open Source software packages, portals Development.
+
+CLOUD COMPUTING
+Cloud computing opportunity is real and redefining markets and player. 
+Airreight believes that cloud is a Service Delivery model for all the Service Providers that would be leveraged with a coexistence with traditional delivery models. 
+Airreight positions as a preferred partner through its set of Cloud Computing services with an aim to help its customers identify the cloud service options that are relevant to their needs. Airreight helps its customers with migration from their existing set-up to private cloud delivery models and offer true Enterprise class service over cloud delivery to meet there business needs.
+
+OBJECTIVE C
+Airreight created a wide range of apps that that supports all apple human interface guidelines and excel in usability as recommended my apple. Our talented team of forecasters with our brilliant and experienced developers can help you to understand requirement, Abstract the app, to understand the user base and their nature, Design, develop and publish the app on the Store and much more.
+At Airreight our designers and Objective-C developers understand the difference in various available platforms and trends, along with the differences across different versions of iOS devices that is available in the market. Though the basic framework does not change, we recognize that the objective, the psychology and hence the user experience for each type of user and the device which they use is unique.
+
+
+
+</div>
+</div>
+<?php include("footer.php"); ?>
+<script src="bootstrap/js/jquery-1.11.2.min.js"></script>  
+<script src="bootstrap/js/bootstrap.js"></script>
+<script type="text/javascript" src="external.js"></script>
+</body>
+</html>
